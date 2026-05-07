e3bfc64d143232aca758e555d2445d3c70110bed
inspirasicerita6-cmyk <inspirasicerita6@gmail.com>
2026-05-07 12:34:02 +0700

    Delete Project Web Tulisan directory

--- Project Web Tulisan/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-10 text-center border-t border-gray-100 bg-white mt-auto">
-      <p className="text-gray-400 text-sm">© 2026 Ruang Sahal.</p>
-    </footer>
-  );
-}
